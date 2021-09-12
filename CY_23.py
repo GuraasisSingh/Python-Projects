4826bcfa3446fa6abbd9318be4d1b8dfe7e17de5
GuraasisSingh <72907179+GuraasisSingh@users.noreply.github.com>
2021-09-12 08:19:25 +0530

    This is the quiz game

---
+++ CY_23.py
@@ -0,0 +1,316 @@
+#Session 23
+#Game Quiz
+consent=int(input("Are you ready to play the game?(yes-1,no-any other number)\n"))
+if consent==1:
+    print("Subject List:1 for Science,2 for Social Science, 3 for Mathematics, 4 for Computers and 5 for General Knowledge\n")
+    s1=int(input("Enter subject number\n"))
+    marks=0
+    if s1==1:
+        print("Science!")
+        q1=input("Enter the smell of hydrogen Sulphide gas:\n")
+        if(q1=="rotten egg"):
+             print("Correct!")
+             marks+=10
+        else:
+             print("Incorrect")
+        q2=float(input("Enter the acceleration due to gravity:\n"))
+        if(q2==9.8):
+            print("Correct!")
+            marks+=10
+        else:
+            print("Incorrect")
+        q3=input("Enter the weigth of cell of humans:\n")
+        if(q3=="1 nanometer"):
+            print("Correct!")
+            marks+=10
+        else:
+            print("Incorrect")
+    elif s1==2:
+        print("Social Science")
+        q1=int(input("Year in which india got Independence\n"))
+        if(q1==1947):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+        q2=float(input("Heigth of mount everest in km\n"))
+        if(q2==8.849):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")        
+        q3=(input("Tell the climate of India:\n"))
+        if(q3=="Tropical Monsoon"):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+    elif s1==3:
+        print("Mathematics")
+        q1=input("Enter coordinates of a 2D graph\n")
+        if q1=="x and y":
+            print("Correct")
+            marks+=10
+        else:
+            print("Incorrect")
+        q2=float(input("(5*2+10-5)/2 \n"))
+        if(q2==7.5):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+        q3=int(input("Enter square root of 104976:\n"))
+        if(q3==324):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+    elif s1==4:
+        print("Computers")
+        q1=input("Which programming language uses pseudo code:\n")
+        if q1=="python":
+            print("Correct")
+            marks+=10
+        else:
+            print("Incorrect")
+        q2=input("Enter the shortcut to copy a file:\n")
+        if(q2=="Ctrl+C"):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+        q3=int(input("Enter storage of int in java in bytes: \n"))
+        if(q3==32):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+    elif s1==5:
+        print("General Knowlegde")
+        q1=input("Name city where 2020 olympics re held:\n")
+        if q1=="tokyo":
+            print("Correct")
+            marks+=10
+        else:
+            print("Incorrect")
+        q2=input("Pongal is a festival of nwhich state:\n")
+        if(q2=="Tamil Nadu"):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+        q3=input("Enter the caital of India:\n")
+        if(q3=="New Delhi"):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+    s2=int(input("Enter subject number\n"))
+    if s2==1:
+        print("Science!")
+        q1=input("Enter the smell of hydrogen Sulphide gas:\n")
+        if(q1=="rotten egg"):
+             print("Correct!")
+             marks+=10
+        else:
+             print("Incorrect")
+        q2=float(input("Enter the acceleration due to gravity:\n"))
+        if(q2==9.8):
+            print("Correct!")
+            marks+=10
+        else:
+            print("Incorrect")
+        q3=input("Enter the weigth of cell of humans:\n")
+        if(q3=="1 nanometer"):
+            print("Correct!")
+            marks+=10
+        else:
+            print("Incorrect")
+    elif s2==2:
+        print("Social Science")
+        q1=int(input("Year in which india got Independence\n"))
+        if(q1==1947):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+        q2=float(input("Heigth of mount everest in km\n"))
+        if(q2==8.849):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")        
+        q3=(input("Tell the climate of India:\n"))
+        if(q3=="Tropical Monsoon"):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+    elif s2==3:
+        print("Mathematics")
+        q1=input("Enter coordinates of a 2D graph\n")
+        if q1=="x and y":
+            print("Correct")
+            marks+=10
+        else:
+            print("Incorrect")
+        q2=float(input("(5*2+10-5)/2 \n"))
+        if(q2==7.5):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+        q3=int(input("Enter square root of 104976:\n"))
+        if(q3==324):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+    elif s2==4:
+        print("Computers")
+        q1=input("Which programming language uses pseudo code:\n")
+        if q1=="python":
+            print("Correct")
+            marks+=10
+        else:
+            print("Incorrect")
+        q2=input("Enter the shortcut to copy a file:\n")
+        if(q2=="Ctrl+C"):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+        q3=int(input("Enter storage of int in java in bytes: \n"))
+        if(q3==32):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+    elif s2==5:
+        print("General Knowlegde")
+        q1=input("Name city where 2020 olympics re held:\n")
+        if q1=="tokyo":
+            print("Correct")
+            marks+=10
+        else:
+            print("Incorrect")
+        q2=input("Pongal is a festival of nwhich state:\n")
+        if(q2=="Tamil Nadu"):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+        q3=input("Enter the caital of India:\n")
+        if(q3=="New Delhi"):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+    s3=int(input("Enter subject number\n"))
+    if s3==1:
+        print("Science!")
+        q1=input("Enter the smell of hydrogen Sulphide gas:\n")
+        if(q1=="rotten egg"):
+             print("Correct!")
+             marks+=10
+        else:
+             print("Incorrect")
+        q2=float(input("Enter the acceleration due to gravity:\n"))
+        if(q2==9.8):
+            print("Correct!")
+            marks+=10
+        else:
+            print("Incorrect")
+        q3=input("Enter the weigth of cell of humans:\n")
+        if(q3=="1 nanometer"):
+            print("Correct!")
+            marks+=10
+        else:
+            print("Incorrect")
+    elif s3==2:
+        print("Social Science")
+        q1=int(input("Year in which india got Independence\n"))
+        if(q1==1947):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+        q2=float(input("Heigth of mount everest in km\n"))
+        if(q2==8.849):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")        
+        q3=(input("Tell the climate of India:\n"))
+        if(q3=="Tropical Monsoon"):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+    elif s3==3:
+        print("Mathematics")
+        q1=input("Enter coordinates of a 2D graph\n")
+        if q1=="x and y":
+            print("Correct")
+            marks+=10
+        else:
+            print("Incorrect")
+        q2=float(input("(5*2+10-5)/2 \n"))
+        if(q2==7.5):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+        q3=int(input("Enter square root of 104976:\n"))
+        if(q3==324):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+    elif s3==4:
+        print("Computers")
+        q1=input("Which programming language uses pseudo code:\n")
+        if q1=="python":
+            print("Correct")
+            marks+=10
+        else:
+            print("Incorrect")
+        q2=input("Enter the shortcut to copy a file:\n")
+        if(q2=="Ctrl+C"):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+        q3=int(input("Enter storage of int in java in bytes: \n"))
+        if(q3==32):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+    elif s3==5:
+        print("General Knowlegde")
+        q1=input("Name city where 2020 olympics re held:\n")
+        if q1=="tokyo":
+            print("Correct")
+            marks+=10
+        else:
+            print("Incorrect")
+        q2=input("Pongal is a festival of nwhich state:\n")
+        if(q2=="Tamil Nadu"):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+        q3=input("Enter the caital of India:\n")
+        if(q3=="New Delhi"):
+            print("Correct!")
+            marks+=10
+        else:
+             print("Incorrect")
+    else:
+        print("Invalid Choice!")
+    percent=(marks/90)*100
+    print("The percentage of marks scored is",percent)
+print("Thank you for playing the game!")
+        
+        
